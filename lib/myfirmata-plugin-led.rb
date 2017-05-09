b42d3425131be00dad03115e9bfd53115187664d
Your Name <you@example.com>
2017-05-09 23:24:30 +0100

    code improvement: The LED methods are now handled by the pinx gem.

--- lib/myfirmata-plugin-led.rb
+++ lib/myfirmata-plugin-led.rb
@@ -4,10 +4,25 @@
 
 # a plugin for the myfirmata gem
 
+require 'pinx'
+
 
 class MyFirmataPluginLed
 
+  class Led < PinX
 
+    def initialize(arduino, id)
+      super(id)
+      @arduino = arduino
+    end
+    
+    def set_pin(val)
+      super(val)
+      @arduino.digital_write @id, val
+    end
+        
+  end
+  
   def initialize(arduino, settings: {}, variables: {})
     
     @arduino = arduino
@@ -29,18 +44,35 @@ class MyFirmataPluginLed
   
   def on_led_message(message)
 
-    r = message.match(/(\d+)\s*(on|off)/)
+    r = message.match(/(\d+)\s*(on|off|blink)\s*([\d\.]+)?(?:\s*duration\s)?([\d\.]+)?/)
 
     if r then
-      
-      index, state = r.captures
-      @arduino.digital_write @pins[index.to_i], state == 'on'
-      
+      index, state, seconds, raw_duration = r.captures
+      duration = raw_duration ? raw_duration.to_f : nil
+
+      a = case state
+
+        when 'on'
+          [:on, duration]
+
+        when 'off'
+          [:off]
+
+        when 'blink'
+          seconds = seconds ? seconds.to_f : 0.5
+          [:blink, seconds, duration: duration]
+      end
+
+      @led[index.to_i].send(*a)
     end
   end  
     
   def start()
-        
+    
+    if @pins.any?  then
+      @led = @pins.map{|x| Led.new @arduino, x}
+    end
+    
   end
   
   alias on_start start
--- myfirmata-plugin-led.gemspec
+++ myfirmata-plugin-led.gemspec
@@ -1,9 +1,10 @@
 Gem::Specification.new do |s|
   s.name = 'myfirmata-plugin-led'
-  s.version = '0.1.0'
-  s.summary = 'myfirmata-plugin-led'
+  s.version = '0.2.0'
+  s.summary = 'A MyFirmata plugin to control 1 or more LEDs via a SimplePubSub subscription.'
   s.authors = ['James Robertson']
-  s.files = Dir['lib/**/*.rb']
+  s.files = Dir['lib/myfirmata-plugin-led.rb']
+  s.add_runtime_dependency('pinx', '~> 0.1', '>=0.1.0')
   s.signing_key = '../privatekeys/myfirmata-plugin-led.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
